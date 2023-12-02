df509bd26a302800da201535303df292a41fd2a0
ekamiraa <ekamiranovitas@gmail.com>
2023-12-02 23:08:24 +0700

    Fecthing Reviews dengan parameternya title buku

--- lib/models/reviews.dart
+++ lib/models/reviews.dart
@@ -1,33 +1,22 @@
 class Reviews {
-  String profile;
-  String name;
-  String article;
-  double rate;
+  final String bookTitle;
+  final String byline;
+  final String summary;
+  final String url;
 
-  Reviews(
-    this.profile,
-    this.name,
-    this.article,
-    this.rate,
-  );
+  Reviews({
+    required this.bookTitle,
+    required this.byline,
+    required this.summary,
+    required this.url,
+  });
 
-  static List<Reviews> reviewBestSeller() {
-    return [
-      Reviews(
-          "profile2.jpeg",
-          "Richard E. Nichollis",
-          "When an enormous transparent dome settles over a small town in Maine in Stephen King’s new novel, it’s just fine with Big Jim, the local tyrant-in-waiting, and his pet goon squad.",
-          4.8),
-      Reviews(
-          "profile3.jpg",
-          "Janet Maslin",
-          "Stephen King’s latest novel, “11/22/63,” tells of a schoolteacher who travels back to 1958 to alter history, and falls in love as well.",
-          4.5),
-      Reviews(
-          "profile1.jpg",
-          "Jane Will",
-          "Stephen King’s time traveler tries to undo some painful history.",
-          5),
-    ];
+  factory Reviews.fromJson(Map<String, dynamic> json) {
+    return Reviews(
+      bookTitle: json['book_title'] ?? "",
+      byline: json['byline'] ?? "",
+      summary: json['summary'] ?? "",
+      url: json['url'] ?? "",
+    );
   }
 }
